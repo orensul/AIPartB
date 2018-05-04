093deb0f77889c430524c0b6b57391179f496ade
Skinfaxi29 <ajbarnett29@gmail.com>
2018-05-04 15:44:02 +1000

    Update RandomPlayer.py

--- RandomPlayer.py
+++ RandomPlayer.py
@@ -1,11 +1,11 @@
 from BoardState import BoardState
+from TileEnum import TileEnum
 from Node import Node
+from MC_Node import MC_Node
 import random
 import copy
-CUT_OFF_DEPTH_LIMIT = 3
-
-# a constant
-INFINITY = 1.0e400
+import time
+INITIAL_CUT_OFF_DEPTH_LIMIT = 2
 
 class Player:
     def __init__(self, colour):
@@ -17,81 +17,30 @@ class Player:
         """
 
         random.seed(9002)
-        self._color = colour
-        self._opponent_color = self.get_opponent_color()
         self._board = BoardState()
+        if(colour == 'white'):
+            self._color = TileEnum.WHITE_PIECE
+            self._opponent_color = TileEnum.BLACK_PIECE
+            self._monte_carlo_tree = MC_Node(copy.deepcopy(self._board), None, self._color, 0, None)
+        elif(colour == 'black'):
+            self._color = TileEnum.BLACK_PIECE
+            self._opponent_color = TileEnum.WHITE_PIECE
+            self._monte_carlo_tree = MC_Node(copy.deepcopy(self._board), None, self._opponent_color, 0, None)
 
-    def get_opponent_color(self):
-        if self._color == 'white':
-            return 'black'
-        return 'white'
+        self._minimax_cutoff_depth = INITIAL_CUT_OFF_DEPTH_LIMIT
 
-    def is_cut_off(self, node):
+    def update(self, action):
         """
-        :param node: node which represents a state of the board
-        :return: boolean - true is node is in the depth of cut-off limit, false - otherwise.
+        This method is called by the referee to inform your player about the opponent’s
+        most recent move, so that you can maintain your internal board configuration.
+        :param action: representation of the opponent’s recent action
+        :return:
         """
-        if node.get_depth() >= CUT_OFF_DEPTH_LIMIT:
-            return True
-        return False
-
-    def minimax_decision(self, operators, turns):
-        max_val = -INFINITY
-        operation = operators[0]
-        alpha = - INFINITY
-        beta = INFINITY
-
-        for op in operators:
-            op_board = copy.deepcopy(self._board)
-            if op_board.get_is_place_phase():
-                row, col = op[0], op[1]
-                op_board.place_piece(self._color, (row, col))
-            else:
-                source, dest = op[0], op[1]
-                source_row, source_col, dest_row, dest_col = source[0], source[1], dest[0], dest[1]
-
-                op_board.move_piece(self._color, source_row, source_col, dest_row, dest_col)
-
-            node = Node(op_board, None, 1, self._opponent_color, turns+1)
-
-            curr_val = self.minimax_value(node, 0, True, alpha, beta)
-            if curr_val > alpha:
-                alpha = curr_val
-                operation = op
-            if curr_val < beta:
-                beta = curr_val
-
-        return operation
-
-    def minimax_value(self, node, depth, is_maximizing_player, alpha, beta):
-
-        if self.is_cut_off(node):
-            eval = node.get_eval(self._color)
-            return eval
-
-        node.expand_successors()
-
-        if is_maximizing_player:
-            best_val = - INFINITY
-            for child in node.get_successors():
-                value = self.minimax_value(child, depth+1, False, alpha, beta)
-                best_val = max(best_val, value)
-                alpha = max(alpha, best_val)
-                if beta <= alpha:
-                    break
-            return best_val
-        else:
-            best_val = INFINITY
-            for child in node.get_successors():
-                value = self.minimax_value(child, depth+1, True, alpha, beta)
-                best_val = min(best_val, value)
-                beta = min(beta, best_val)
-                if beta <= alpha:
-                    break
-            return best_val
-
-
-
+        if not action == None:
+            action = self.switch_row_column(action)
+            self._board.take_action(action, self._opponent_color)
+        #if self._board.get_is_place_phase():
+            #self._monte_carlo_tree = self._monte_carlo_tree.get_child_from_move(action)
 
 
     def action(self, turns):
@@ -103,41 +52,91 @@ class Player:
                  (x,y) -  placing a piece on square (x,y)
                  ((a,b),(c,d)) -  moving a piece from square (a,b) to square (c,d)
         """
-        self._board.check_shrink_board(turns)
-        if self._board.get_is_place_phase():
-            coords_list = self._board.get_empty_tiles(self._color)
-            #coord = self.minimax_decision(coords_list, turns)
-            coord = coords_list[random.randint(0, len(coords_list) - 1)]
-            row, col = coord[0], coord[1]
-            self._board.place_piece(self._color, (row, col))
-            return_val = col, row
+        actions = self._board.get_actions(turns,self._color)
+        if len(actions) > 0:
+            if self._board.get_is_place_phase():
+                action = actions[random.randint(0, len(actions) - 1)]
+                #action = self.minimax_decision(actions, turns)
+            else:
+                #action = self.minimax_decision(actions, turns)
+                action = actions[random.randint(0, len(actions) - 1)]
 
+
+            self._board.take_action(action, self._color)
+            self._board.check_update_phase(turns)
+            return self.switch_row_column(action)
         else:
-            coords_list = self._board.get_available_moves(self._color)
-            #coord = self.minimax_decision(coords_list, turns)
+            self._board.check_update_phase(turns)
+            return None
 
-            coord = coords_list[random.randint(0, len(coords_list) - 1)]
-            source, dest = coord[0], coord[1]
-            source_row, source_col, dest_row, dest_col = source[0], source[1], dest[0], dest[1]
 
-            self._board.move_piece(self._color, source_row, source_col, dest_row, dest_col)
-
-            return_val = (source_col, source_row), (dest_col, dest_row)
-
-        self._board.check_update_phase(turns)
-
-        return return_val
-
-    def update(self, action):
+    def is_cut_off(self, node):
         """
-        This method is called by the referee to inform your player about the opponent’s
-        most recent move, so that you can maintain your internal board configuration.
-        :param action: representation of the opponent’s recent action
-        :return:
+        :param node: node which represents a state of the board
+        :return: boolean - true is node is in the depth of cut-off limit, false - otherwise.
         """
+        if node.get_depth() >= self._minimax_cutoff_depth:
+            return True
+        return False
 
-        if not isinstance(action[0], tuple):
-            self._board.place_piece(self._opponent_color, (action[1], action[0]))
+
+
+    def minimax_decision(self, operators, turns):
+        if len(operators) == 0:
+            return None
+        operation = operators[0]
+        alpha = -10000
+        beta = 10000
+        for op in operators:
+            #print(alpha)
+            #print(beta)
+            #print('-----------------------')
+            op_board = copy.deepcopy(self._board)
+
+            op_board.take_action(op, self._color)
+            op_board.check_update_phase(turns)
+
+            node = Node(op_board, None, 1, self._opponent_color, turns+1)
+
+            curr_val= self.min_value(node,alpha,beta)
+            if curr_val > alpha:
+                alpha = curr_val
+                operation = op
+
+        return operation
+
+    def max_value(self, node,alpha,beta):
+        """
+        :param node: node which represents a state of the board
+        :return: evaluation function result
+        """
+        if self.is_cut_off(node):
+            return node.get_eval(self._color)
         else:
-            self._board.remove_piece(self._opponent_color, (action[0][1], action[0][0]))
-            self._board.place_piece(self._opponent_color, (action[1][1], action[1][0]))
+            node.expand_successors()
+            for child in node.get_successors():
+                alpha = max([alpha,self.min_value(child,alpha,beta)])
+                if alpha >= beta:
+                    return beta
+            return alpha
+
+    def min_value(self, node,alpha,beta):
+        """
+        :param node: node which represents a state of the board
+        :return: evaluation function result
+        """
+        if self.is_cut_off(node):
+            return node.get_eval(self._color)
+        else:
+            node.expand_successors()
+            for child in node.get_successors():
+                beta = min([beta,self.max_value(child,alpha,beta)])
+                if beta <= alpha:
+                    return alpha
+            return beta
+
+    def switch_row_column(self, action):
+        if isinstance(action[0], tuple):
+            return (action[0][::-1],action[1][::-1])
+        else:
+            return action[::-1]
