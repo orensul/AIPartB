743a676a79f2ba0529d4e43e0fabbece2b00befb
Skinfaxi29 <ajbarnett29@gmail.com>
2018-05-08 20:07:19 +1000

    Refactored, iterative alpha-beta with distant node cutoff and time control. Evaluation function defined but needs tuning

--- Node.py
+++ Node.py
@@ -1,41 +1,48 @@
 import copy
-import random
+import math
+import queue
+
+INFINITY = 100000
+NEG_INFINITY = -100000
+UNEXPANDED_NODE_QUEUE_PRIORITY = 0
 class Node:
     """
     This class of Node contains the information of node in the search tree.
     each node contains board_state, his parent, successors.
     """
-    def __init__(self, board_state, parent, depth, color, turn):
+    def __init__(self, board_state, color, depth, turn, action, alpha, beta):
 
-        random.seed(9002)
         # current state of the board this node represents
         self._board = board_state
-        # parent node, used to traverse up sequence of nodes once goal state found
-        self._parent = parent
         # collection of successor nodes
-        self._successors = []
+        self._successors = queue.PriorityQueue()
         # depth of the node in the tree, to know stop expanding because of cut-off
         self._depth = depth
         self._color = color
         self._turn = turn
+        self._action = action
+        self._alpha = alpha
+        self._beta = beta
 
-    def get_color(self):
-        return self._color
+    def __lt__(self, other):
+        """Override the default Unequal behavior"""
+        if any([self == None, other == None]):
+            return False
+        else:
+            return self._action < other._action
+
+    def set_alpha_beta(self, alpha, beta):
+        self._alpha = alpha
+        self._beta = beta
+
+
     def get_eval(self, color):
-        #return  random.randint(5,200000) +random.randint(5,2000)
-        return self._board.rank_centre_control(color) + 0*len(self._board.get_available_moves(color))+18*(self._board.get_remaining_pieces(color)-self._board.get_remaining_pieces(self._board.get_opposite_color(color)))
-    def get_board(self):
-        return self._board
-    def get_depth(self):
-        return self._depth
-    def get_turn(self):
-        return self._turn
-    def get_successors(self):
-        return self._successors
-    def get_parent(self):
-        return self._parent
+        return self._board.evaluation(color,self._turn)
 
-    def expand_successors(self):
+
+
+    def expand_successors(self, eval_color):
+        self._board.check_shrink_board(self._turn)
         actions = self._board.get_actions(self._turn,self._color)
         if len(actions) > 0:
             for action in actions:
@@ -45,4 +52,77 @@ class Node:
                     new_board.take_action(action, self._color)
                     new_board.check_update_phase(self._turn)
                     # update successors with the new board state
-                    self._successors.append(Node(new_board, self, self._depth + 1, new_board.get_opposite_color(self._color),self._turn + 1))
+                    child = Node(new_board, new_board.get_opposite_color(self._color), self._depth + 1, self._turn + 1, action, self._alpha, self._beta)
+                    self._successors.put((UNEXPANDED_NODE_QUEUE_PRIORITY,child))
+
+
+
+
+    def min_max_value(self,cutoff_depth, eval_color):
+        if self._depth >= cutoff_depth:
+            return (self.get_eval(eval_color),self._action)
+        else:
+            successors_update = queue.PriorityQueue()
+
+            if self._successors.empty():
+                 self.expand_successors(self._color)
+                 if self._successors.empty():
+                     return (self.get_eval(eval_color),self._action)
+
+            if self._color == eval_color:
+                best_action = self._action
+                expanded_count = 0
+                late_move_reduction_cutoff = math.floor(self._successors.qsize()/((cutoff_depth - self._depth)))
+                while not self._successors.empty():
+                    child = self._successors.get()[1]
+                    (score,action) = child.min_max_value(cutoff_depth, eval_color)
+                    child.set_alpha_beta(self._alpha,self._beta)
+                    successors_update.put((-score,child))
+
+                    if score >= self._beta:
+                        while not self._successors.empty():
+                            successors_update.put(self._successors.get())
+                        self._successors = successors_update
+                        return (self._beta, self._action)
+
+                    if score > self._alpha:
+                        self._alpha = score
+                        best_action = action
+
+                    expanded_count +=1
+                    if expanded_count >= late_move_reduction_cutoff:
+                        while not self._successors.empty():
+                            successors_update.put(self._successors.get())
+                        break
+                self._successors = successors_update
+                if self._depth == 0:
+                    return (self._alpha, best_action)
+                else:
+                    return (self._alpha, self._action)
+
+            else:
+                expanded_count = 0
+                late_move_reduction_cutoff = math.floor(self._successors.qsize()/((cutoff_depth - self._depth)))
+                while not self._successors.empty():
+                    child = self._successors.get()[1]
+                    (score, action) = child.min_max_value(cutoff_depth, eval_color)
+                    child.set_alpha_beta(self._alpha,self._beta)
+                    successors_update.put((-score,child))
+
+                    if score <= self._alpha:
+                        while not self._successors.empty():
+                            successors_update.put(self._successors.get())
+                        self._successors = successors_update
+                        return (self._alpha, self._action)
+
+                    if score < self._beta:
+                        self._beta = score
+
+                    expanded_count +=1
+                    if expanded_count >= late_move_reduction_cutoff:
+                        while not self._successors.empty():
+                            successors_update.put(self._successors.get())
+                        break
+
+                self._successors = successors_update
+                return (self._beta, self._action)
