c1816e218f9ffe1064e28724d415bc2798cc109e
Skinfaxi29 <ajbarnett29@gmail.com>
2018-05-08 20:05:12 +1000

    Refactored, iterative alpha-beta with distant node cutoff and time control. Evaluation function defined but needs tuning.

--- Player.py
+++ Player.py
@@ -1,68 +1,90 @@
+
 from BoardState import BoardState
 from TileEnum import TileEnum
 from Node import Node
-from MC_Node import MC_Node
-import random
 import copy
 import time
-INITIAL_CUT_OFF_DEPTH_LIMIT = 2
+import math
+
+'''
+Player class controller class for an adversarial search AI designed to compete against
+other similar programs in a game of 'Watch Your Back' through the referee.py class. The overall
+program plays the game making decisions using an alpha-beta pruning minimax algorithm with heuristic
+alterations.
+The class is mainly a facade providing access to the actual logical functionality defined within
+the BoardState (internal board representation) and Node (minimax information) classes. The Player class
+also measures and tracks time to support staying within the project constraints of 120 sec total decision
+time per player per game.
+'''
+
+INFINITY = 100000
+NEG_INFINITY = -100000
+'''in sec accounting for other player'''
+MAX_GAME_TIME = 240
+'''estimate for maximum moves a this player should make in the game. ~12 in placing ~100 max in moving'''
+MAX_MOVES = 112
+'''estimate of games average branching factor. Allows prediction of how long the next alpha-beta depth iteration will take'''
+GAME_AVERAGE_BRANCHING_FACTOR = 40
 
 class Player:
     def __init__(self, colour):
         """
         called by the referee once at the beginning of the game to initialise.
-        Here we will set the state of the board and more states we will want to maintain during the game.
-        :param colour:  string representing the piece colour your program will control for this game.
-        can be 'white' or 'black
+        Here we initialise the internal state of the board and set the team colour for this player.
+        :param colour:  string representing the piece colour this class will control for this game.
+        can be 'white' or 'black'
         """
-
-        random.seed(9002)
+        #random.seed(9002)
+        '''blank 8x8 board with corners'''
         self._board = BoardState()
+        '''Using the TileEnum instead of the string for color allows us to avoid switching in BoardState Class'''
         if(colour == 'white'):
             self._color = TileEnum.WHITE_PIECE
             self._opponent_color = TileEnum.BLACK_PIECE
-            #self._monte_carlo_tree = MC_Node(copy.deepcopy(self._board), None, self._color, 0, None)
         elif(colour == 'black'):
             self._color = TileEnum.BLACK_PIECE
             self._opponent_color = TileEnum.WHITE_PIECE
-            #self._monte_carlo_tree = MC_Node(copy.deepcopy(self._board), None, self._opponent_color, 0, None)
 
-        self._minimax_cutoff_depth = INITIAL_CUT_OFF_DEPTH_LIMIT
+        self._game_start_time = time.time()
+        self._max_time_for_decision = MAX_GAME_TIME/MAX_MOVES
 
     def update(self, action):
         """
-        This method is called by the referee to inform your player about the opponent’s
-        most recent move, so that you can maintain your internal board configuration.
-        :param action: representation of the opponent’s recent action
+        This method is called by the referee to inform the player about the opponent’s
+        most recent move, maintains the nternal board configuration by applying the
+        opponents action to the BoardState.
+        :param action: representation of the opponent’s recent action tuple for placing, tuple of tuples for moving
         :return:
         """
         if not action == None:
             action = self.switch_row_column(action)
             self._board.take_action(action, self._opponent_color)
-        #if self._board.get_is_place_phase():
-            #self._monte_carlo_tree = self._monte_carlo_tree.get_child_from_move(action)
+
 
 
     def action(self, turns):
         """
-        This method is called by the referee to request an action by your player.
+        Called by the referee to request an action from the player.
         :param turns: turns is an integer representing the number of turns that have
-        taken place since the start of the current game phase
+        taken place since the start of the current game phase, counts from 0-23 in place phase,
+        then 0-... in the moving phase.
         :return: the next action of the player in a format of:
                  (x,y) -  placing a piece on square (x,y)
                  ((a,b),(c,d)) -  moving a piece from square (a,b) to square (c,d)
         """
-        actions = self._board.get_actions(turns,self._color)
-        if len(actions) > 0:
-            if self._board.get_is_place_phase():
-                #action = actions[random.randint(0, len(actions) - 1)]
-                action = self.minimax_decision(actions, turns)
-            else:
-                action = self.minimax_decision(actions, turns)
-                #action = actions[random.randint(0, len(actions) - 1)]
 
+        '''Determine from how much time has passed of the total allowed and the number of likely moves
+        the player has left to make, how much time we should allocate to the next decision.'''
+        self._max_time_for_decision = (MAX_GAME_TIME - (time.time() - self._game_start_time))/((MAX_MOVES-((turns+1)/2)))   #/2 to account for only this players game time
+        '''Check if the internal board should be shrunk on this turn'''
+        self._board.check_shrink_board(turns)
 
+        action = self.minimax_decision(turns,time.time())
+
+        if not action == None:
+            '''update board with action and return action in (col,row) format the referee prefers'''
             self._board.take_action(action, self._color)
+            '''update internal board representation to the move phase if appropriate'''
             self._board.check_update_phase(turns)
             return self.switch_row_column(action)
         else:
@@ -70,72 +92,51 @@ class Player:
             return None
 
 
-    def is_cut_off(self, node):
+    def minimax_decision(self, turns, start_time):
         """
-        :param node: node which represents a state of the board
-        :return: boolean - true is node is in the depth of cut-off limit, false - otherwise.
+        Performs an iterative deepening version of alpha-beta minimax algorithm
+        before the start of each iteration we predict (from the time complexity of
+        alpha-beta and our estimated branching factor) how long the next iteration
+        will take. If we have not allowed the time to perform this for this move
+        the result of the last depth iteration is returned
+
+        :param turs: turns is an integer representing the number of turns that have
+        taken place since the start of the current game phase, counts from 0-23 in place phase,
+        then 0-... in the moving phase.
+        :param start_time: time at which this method was first called
+        :return: the next action of the player in a format of:
+                 (x,y) -  placing a piece on square (x,y)
+                 ((a,b),(c,d)) -  moving a piece from square (a,b) to square (c,d)
         """
-        if node.get_depth() >= self._minimax_cutoff_depth:
-            return True
-        return False
+        root = Node(copy.deepcopy(self._board), self._color, 0, turns, None, NEG_INFINITY, INFINITY)
+        last_iteration_time = 0
+        for depth in range(1,8):
+            #print(self._max_time_for_decision)
+
+            if (self._max_time_for_decision - (time.time()-start_time)) < math.sqrt(GAME_AVERAGE_BRANCHING_FACTOR) * last_iteration_time:
+                pass
+            else:
+                begin = time.time()
+                #print(time.time()-start_time)
+                (alpha,best_action) = root.min_max_value(depth, self._color)
+                last_iteration_time = time.time() - begin
+                #print('DEPTH: ' + str(depth))
+                #print('ALPHA: ' + str(alpha))
+                #print('ACTION: ' + str(best_action))
+                root.set_alpha_beta(NEG_INFINITY,INFINITY)
+                #print('Time spent on decision so far: ' +str(time.time()-start_time))
+        return best_action
 
 
 
-    def minimax_decision(self, operators, turns):
-        if len(operators) == 0:
-            return None
-        operation = operators[0]
-        alpha = -10000
-        beta = 10000
-        for op in operators:
-            #print(alpha)
-            #print(beta)
-            #print('-----------------------')
-            op_board = copy.deepcopy(self._board)
-
-            op_board.take_action(op, self._color)
-            op_board.check_update_phase(turns)
-
-            node = Node(op_board, None, 1, self._opponent_color, turns+1)
-
-            curr_val= self.min_value(node,alpha,beta)
-            if curr_val > alpha:
-                alpha = curr_val
-                operation = op
-
-        return operation
-
-    def max_value(self, node,alpha,beta):
-        """
-        :param node: node which represents a state of the board
-        :return: evaluation function result
-        """
-        if self.is_cut_off(node):
-            return node.get_eval(self._color)
-        else:
-            node.expand_successors()
-            for child in node.get_successors():
-                alpha = max([alpha,self.min_value(child,alpha,beta)])
-                if alpha >= beta:
-                    return beta
-            return alpha
-
-    def min_value(self, node,alpha,beta):
-        """
-        :param node: node which represents a state of the board
-        :return: evaluation function result
-        """
-        if self.is_cut_off(node):
-            return node.get_eval(self._color)
-        else:
-            node.expand_successors()
-            for child in node.get_successors():
-                beta = min([beta,self.max_value(child,alpha,beta)])
-                if beta <= alpha:
-                    return alpha
-            return beta
 
     def switch_row_column(self, action):
+        """
+        internal board uses (row,column) format for peice coordinates,
+        referee uses (column,row). Simple method to convert between the two
+        :param action: representation of the opponent’s recent action tuple for placing, tuple of tuples for moving
+        :return:the action of the player in the reversed format
+        """
         if isinstance(action[0], tuple):
             return (action[0][::-1],action[1][::-1])
         else:
