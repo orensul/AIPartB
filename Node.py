5a9237194b0c6bf6a7a240e056700bfcc9bc6e4a
Skinfaxi29 <ajbarnett29@gmail.com>
2018-05-09 18:09:55 +1000

    Changed from priority queue to heapq for successors. A few logical changes.

--- Node.py
+++ Node.py
@@ -1,6 +1,6 @@
 import copy
 import math
-import queue
+from heapq import heappush, heappop
 
 INFINITY = 100000
 NEG_INFINITY = -100000
@@ -15,7 +15,7 @@ class Node:
         # current state of the board this node represents
         self._board = board_state
         # collection of successor nodes
-        self._successors = queue.PriorityQueue()
+        self._successors = []
         # depth of the node in the tree, to know stop expanding because of cut-off
         self._depth = depth
         self._color = color
@@ -25,11 +25,13 @@ class Node:
         self._beta = beta
 
     def __lt__(self, other):
-        """Override the default Unequal behavior"""
-        if any([self == None, other == None]):
-            return False
-        else:
-            return self._action < other._action
+        return False
+
+    def get_successors(self):
+        return self._successors
+
+    def get_action(self):
+        return self._action
 
     def set_alpha_beta(self, alpha, beta):
         self._alpha = alpha
@@ -44,85 +46,91 @@ class Node:
     def expand_successors(self, eval_color):
         self._board.check_shrink_board(self._turn)
         actions = self._board.get_actions(self._turn,self._color)
+        #print(actions)
         if len(actions) > 0:
             for action in actions:
-                    # create copy of this node's board
-                    new_board = copy.deepcopy(self._board)
-                    # update the copy with the action
-                    new_board.take_action(action, self._color)
-                    new_board.check_update_phase(self._turn)
-                    # update successors with the new board state
-                    child = Node(new_board, new_board.get_opposite_color(self._color), self._depth + 1, self._turn + 1, action, self._alpha, self._beta)
-                    self._successors.put((UNEXPANDED_NODE_QUEUE_PRIORITY,child))
+                # create copy of this node's board
+                new_board = copy.deepcopy(self._board)
+                # update the copy with the action
+                new_board.take_action(action, self._color)
+                new_board.check_update_phase(self._turn)
+                # update successors with the new board state
+                child = Node(new_board, new_board.get_opposite_color(self._color), self._depth + 1, self._turn + 1, action, self._alpha, self._beta)
+                heappush(self._successors,(UNEXPANDED_NODE_QUEUE_PRIORITY,child))
+            #print([(key[0],key[1].get_action()) for key in self._successors.queue])
+
 
 
 
 
     def min_max_value(self,cutoff_depth, eval_color):
         if self._depth >= cutoff_depth:
-            return (self.get_eval(eval_color),self._action)
+            '''print(eval_color)
+            print(self._turn)
+            print(self.get_eval(eval_color))
+            self._board.print_board()'''
+            return self.get_eval(eval_color)
         else:
-            successors_update = queue.PriorityQueue()
+            successors_update = []
 
-            if self._successors.empty():
+            if self._successors == []:
                  self.expand_successors(self._color)
-                 if self._successors.empty():
-                     return (self.get_eval(eval_color),self._action)
+                 if self._successors == []:
+                     return (self.get_eval(eval_color))
 
             if self._color == eval_color:
-                best_action = self._action
                 expanded_count = 0
-                late_move_reduction_cutoff = math.floor(self._successors.qsize()/((cutoff_depth - self._depth)))
-                while not self._successors.empty():
-                    child = self._successors.get()[1]
-                    (score,action) = child.min_max_value(cutoff_depth, eval_color)
+                late_move_reduction_cutoff = math.floor(len(self._successors)/((cutoff_depth - self._depth)))
+                while not self._successors == []:
+                    child = heappop(self._successors)[1]
+                    score = child.min_max_value(cutoff_depth, eval_color)
+
+
                     child.set_alpha_beta(self._alpha,self._beta)
-                    successors_update.put((-score,child))
+                    #print(-score,child.get_action())
+                    heappush(successors_update,(-score,child))
 
                     if score >= self._beta:
-                        while not self._successors.empty():
-                            successors_update.put(self._successors.get())
+                        while not self._successors == []:
+                            heappush(successors_update,heappop(self._successors))
                         self._successors = successors_update
-                        return (self._beta, self._action)
+                        return self._beta
 
                     if score > self._alpha:
                         self._alpha = score
-                        best_action = action
 
                     expanded_count +=1
                     if expanded_count >= late_move_reduction_cutoff:
-                        while not self._successors.empty():
-                            successors_update.put(self._successors.get())
+                        while not self._successors == []:
+                            heappush(successors_update,heappop(self._successors))
                         break
                 self._successors = successors_update
-                if self._depth == 0:
-                    return (self._alpha, best_action)
-                else:
-                    return (self._alpha, self._action)
+                return self._alpha
 
             else:
                 expanded_count = 0
-                late_move_reduction_cutoff = math.floor(self._successors.qsize()/((cutoff_depth - self._depth)))
-                while not self._successors.empty():
-                    child = self._successors.get()[1]
-                    (score, action) = child.min_max_value(cutoff_depth, eval_color)
+                late_move_reduction_cutoff = math.floor(len(self._successors)/((cutoff_depth - self._depth)))
+                while not self._successors == []:
+                    child = heappop(self._successors)[1]
+                    score = child.min_max_value(cutoff_depth, eval_color)
                     child.set_alpha_beta(self._alpha,self._beta)
-                    successors_update.put((-score,child))
+                    #print(-score,child.get_action())
+                    heappush(successors_update,(score,child))
 
                     if score <= self._alpha:
-                        while not self._successors.empty():
-                            successors_update.put(self._successors.get())
+                        while not self._successors == []:
+                            heappush(successors_update,heappop(self._successors))
                         self._successors = successors_update
-                        return (self._alpha, self._action)
+                        return self._alpha
 
                     if score < self._beta:
                         self._beta = score
 
                     expanded_count +=1
                     if expanded_count >= late_move_reduction_cutoff:
-                        while not self._successors.empty():
-                            successors_update.put(self._successors.get())
+                        while not self._successors == []:
+                            heappush(successors_update,heappop(self._successors))
                         break
 
                 self._successors = successors_update
-                return (self._beta, self._action)
+                return self._beta
